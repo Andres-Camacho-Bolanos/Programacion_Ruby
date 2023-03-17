461cb391e09a9ce6ef2d8da03e8783cd32b2f8bb
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-03-17 00:42:18 -0600

    Create string_length.rb

---
+++ string_length.rb
@@ -0,0 +1,10 @@
+#string length
+
+nombre = 'Curso de Ruby :)'
+
+=begin
+El método de clase .length nos va a devolver el tamaño del string (Cadena de caracteres).
+El espacio también cuenta como caracter.
+=end
+
+puts nombre.length
