e445c1b73af48c6ee76f6dfa1c7f1ccf0a47537a
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-03-17 00:17:15 -0600

    Create interpretación_dinamica.rb

---
+++ interpretación_dinamica.rb
@@ -0,0 +1,12 @@
+=begin
+Ruby es un tipo de lenguaje de programación debilmente tipado
+No se requiere especificar el tipo de dato de una variable o constante, a menos que vayamos a cambiarlo posteriormente
+=end
+
+#Para saber que tipo de dato es el que pusimos, se pone .class
+
+variable = 5
+
+puts variable.class
+
+#Nos va indicar que es entero.
