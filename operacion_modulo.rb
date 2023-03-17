bf4c8f2017a7bc8a566517918ad3ed8d7cef4f02
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-03-17 00:35:04 -0600

    Create operacion_modulo.rb

---
+++ operacion_modulo.rb
@@ -0,0 +1,12 @@
+#Operador con módulo
+
+puts 30 / 7
+
+=begin
+Como podemos ver, en una división normal nos devolverá como resultado cuantas veces cabe el 30 en el 7
+dependiendo de si el valor es entero o flotante.
+=end
+
+#Por otra parte, la operación de módulo nos va a devolver el residuo de una división.
+
+puts 30 % 7
